588b47acc1ebeb3a4fc9ba903b40bb16a18b156c
renatronic <renatronic@protonmail.com>
2021-04-23 11:15:24 -0300

    def you_pick createad

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -38,18 +38,71 @@ def play():
     button_paper = Button(root, image = hand_paper_img, command = lambda:you_pick('paper'))
     button_scissors = Button(root, image = hand_scissors_img, command = lambda:you_pick('scissors'))
 
-    button_rock.grid(root, row = 0, column = 0)
-    button_paper.grid(root, row = 0, column = 1)
-    button_scissors.grid(root, row = 0, column = 2)
+    button_rock.grid(row = 0, column = 0)
+    button_paper.grid(row = 0, column = 1)
+    button_scissors.grid(row = 0, column = 2)
 
 def computer_pick():
     choice = random.choice(['rock', 'paper', 'scissors'])
     return choice
 
-def you_pick():
+def you_pick(your_choice):
     global click
 
     comp_pick = computer_pick()
 
+    if click == True:
+        if your_choice == 'rock':
+            button_rock.configure(image = rock_img)
+            if comp_pick == 'rock':
+                button_paper.configure(image = rock_img)
+                button_scissors.configure(image = tie_img)
+                click = False
+            elif comp_pick == 'paper':
+                button_paper.configure(image = paper_img)
+                button_scissors.configure(image = loose_img)
+                click = False
+            else:
+                button_paper.configure(image = scissors_img)
+                button_scissors.configure(image = win_img)
+                click = False
+
+        elif your_choice == 'paper':
+            button_paper.configure(image = paper_img)
+            if comp_pick == 'rock':
+                button_rock.configure(image = rock_img)
+                button_scissors.configure(image = win_img)
+                click = False
+            elif comp_pick == 'paper':
+                button_rock.configure(image = paper_img)
+                button_scissors.configure(image = tie_img)
+                click = False
+            else:
+                button_rock.configure(image = scissors_img)
+                button_scissors.configure(image = loose_img)
+                click = False
+                
+        elif your_choice == 'scissors':
+            button_scissors.configure(image = scissors_img)
+            if comp_pick == 'rock':
+                button_paper.configure(image = rock_img)
+                button_rock.configure(image = loose_img)
+                click = False
+            elif comp_pick == 'paper':
+                button_paper.configure(image = paper_img)
+                button_rock.configure(image = win_img)
+                click = False
+            else:
+                button_paper.configure(image = scissors_img)
+                button_rock.configure(image = tie_img)
+                click = False
+    else:
+        if your_choice == 'rock' or your_choice == 'paper' or your_choice == 'scissors':
+            button_rock.configure(image = hand_rock_img)
+            button_paper.configure(image = hand_paper_img)
+            button_scissors.configure(image = hand_scissors_img)
+            click = True
+
+play()
 
 root.mainloop()
